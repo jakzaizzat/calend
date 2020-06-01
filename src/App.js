116a4f56f200fa7f92f0fb0ea0b666cd4b1ebbe0
Jakz Aizzat <jakzaizzat@gmail.com>
2020-06-01 11:08:08 +0800

    add eslint n prettier

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const list = [
+  {
+    id: 1,
+    title: "30 minutes consultation"
+  },
+  {
+    id: 1,
+    title: "30 minutes consultation"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <button className="rounded px-4 py-2 bg-red-800 text-white">Click me</button>
+      <button className="rounded px-4 py-2 bg-red-800 text-white">
+        Click me
+      </button>
     </div>
   );
 }
