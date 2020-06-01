94467a98e9af2cc8557822992d416527830be068
Jakz Aizzat <jakzaizzat@gmail.com>
2020-06-01 22:16:24 +0800

    Convert to Event component

--- src/components/Event/EventList.jsx
+++ src/components/Event/EventList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Card from "../shared/Card";
 import BaseButton from "../shared/BaseButton";
+import Event from "./Event";
 
 const EventList = ({ events }) => {
   return (
@@ -12,36 +13,7 @@ const EventList = ({ events }) => {
         >
           <ul>
             {events.map(event => (
-              <li key={event.id}>
-                <span className="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
-                  <div className="px-4 py-4 sm:px-6">
-                    <div className="flex items-center justify-between">
-                      <div className="text-sm leading-5 font-medium text-indigo-600 truncate">
-                        {event.title}
-                      </div>
-                      <div className="ml-2 flex-shrink-0 flex">
-                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
-                          Active
-                        </span>
-                      </div>
-                    </div>
-                    <div className="mt-2 sm:flex sm:justify-between">
-                      <div className="sm:flex">
-                        <div className="mr-6 flex items-center text-sm leading-5 text-gray-500">
-                          <svg
-                            className="flex-shrink-0 mr-2 h-5 w-5 text-gray-400"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                          >
-                            <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
-                          </svg>
-                          One on One
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </span>
-              </li>
+              <Event key={event.id} title={event.title} />
             ))}
           </ul>
         </Card>
