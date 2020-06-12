c5ab2f150819df5e2e316c7b26fc9a3ac420df5b
Jakz Aizzat <jakzaizzat@gmail.com>
2020-06-12 11:24:10 +0800

    Initialize event on auth page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { useEffect, useMemo, useState } from "react";
 import Navigation from "./components/shared/Navigation";
 import { UserContext } from "./state/UserContext";
-import "toasted-notes/src/styles.css";
-import "./App.css";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { getEvents } from "./store/modules/event/actions";
+import * as api from "./api/events-api-mock";
+
 import mock from "./config/mock";
 import Routes from "./router/routes";
 
-const App = () => {
+import "toasted-notes/src/styles.css";
+import "./App.css";
+
+const App = (props) => {
   const [auth, setAuth] = useState(null);
   const providerAuth = useMemo(() => ({ auth, setAuth }), [auth, setAuth]);
   useEffect(() => {
     if (localStorage.getItem("token")) {
       setAuth(mock.user);
+      api.findAll().then((res) => {
+        let events = JSON.parse(localStorage.getItem("events")) || [];
+        console.log(events);
+        props.getEvents(events);
+      });
     }
   }, []);
 
@@ -27,4 +38,12 @@ const App = () => {
   );
 };
 
-export default App;
+function mapStateToProps({ eventState }) {
+  return { eventState };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ getEvents }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
