85c9ff0b2565de1321b904eac3311c189596c74e
Jakz Aizzat <jakzaizzat@gmail.com>
2020-06-14 07:07:18 +0800

    Remove redux debugger

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./modules";
-const store = createStore(
-  rootReducer,
-  undefined,
-  applyMiddleware(thunk) + window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-
+const store = createStore(rootReducer, undefined, applyMiddleware(thunk));
+// + window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 export default store;
